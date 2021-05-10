26be1442223670024ddd30dcfc8355e1bb8e4cbe
Brendan Smith <brendan5320@gmail.com>
2021-05-09 23:37:34 -0700

    built out message queue for delivered event

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -14,20 +14,40 @@ const handlers = require('./vendor-handler.js');
 const host = process.env.SERVER_URL || 'http://localhost:3000';
 const socket = io.connect(`${host}/caps`);
 
+let order = {
+  store: storeID,
+  orderID: faker.datatype.uuid(),
+  customer: faker.name.findName(),
+  address: faker.address.streetAddress()
+};
+
 // join room 'STOREID'
 socket.emit('join', storeID);
 
+// request all queued messages
+socket.emit('getAll', storeID);
+
 // listeners
 socket.on('delivered', handlers.thankYou);
+socket.on('order-update', (payload) => {
+  console.log(payload);
+  socket.emit('received', payload);
+})
 
-setInterval( () => {
-  // instantiate a new order event every 5 seconds
-  let order = {
-    store: storeID,
-    orderID: faker.datatype.uuid(),
-    customer: faker.name.findName(),
-    address: faker.address.streetAddress()
-  };
+socket.emit('pickup', order);
+
+socket.on('added', () => {
+  socket.disconnect();
+})
+
+// setInterval( () => {
+//   // instantiate a new order event every 5 seconds
+//   let order = {
+//     store: storeID,
+//     orderID: faker.datatype.uuid(),
+//     customer: faker.name.findName(),
+//     address: faker.address.streetAddress()
+//   };
   
-  socket.emit('pickup', order);
-}, 5000);
+//   socket.emit('pickup', order);
+// }, 5000);
